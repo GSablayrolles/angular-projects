f3c3a252e66f694273ab44a69da0e7c2e9398047
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-07-27 18:16:19 +0200

    chore: init angular projects with a debbuging project

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_24
+              nodePackages."@angular/cli"
+              nodePackages.prettier
+            ];
+          };
+        };
+    };
+}
