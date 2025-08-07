e1b54bd880e54e65373be8669365870d06583e17
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-08-07 12:08:33 +0200

    feat: disable emulated shadow dom to use global style of the selectors

--- component-template-learning/src/app/shared/control/control.component.ts
+++ component-template-learning/src/app/shared/control/control.component.ts
@@ -1,11 +1,13 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-control',
     standalone: true,
     imports: [],
     templateUrl: './control.component.html',
-    styleUrl: './control.component.css'
+    styleUrl: './control.component.css',
+    // DIsable the emulation of the shadow dom
+    encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
     label = input.required<string>();
